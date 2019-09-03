66cc477645320fd0202fd5e04ee5a207a72b6480
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-09-03 16:23:02 +0200

    add tsibble package demo

---
+++ r-cheats/package-demos/tsibble.R
@@ -0,0 +1,38 @@
+# ---- dependecies ----
+devtools::install_github("tidyverts/tsibble")
+
+# ---- tsibble-demo ----
+sdd <-
+  data.frame(
+    state = "delaware",
+    city = rep(c("city 1", "city 2"), 2),
+    time_stamp = c("2000-01-01 00:00:00", "2000-01-01 00:00:02", "2000-01-01 00:00:04", "2000-01-01 00:00:06"),
+    sensor = 1:4,
+    stringsAsFactors = FALSE
+  ) %>%
+  dplyr::mutate_at("time_stamp", as.POSIXct) %>%
+  tsibble::as_tsibble(index = "time_stamp", key = "city")
+
+sdd %>% tsibble::index()
+sdd %>% tsibble::index_var()
+sdd %>% tsibble::interval()
+sdd %>%
+  tsibble::index() %>%
+  class()
+sdd %>%
+  tsibble::index_var() %>%
+  class()
+sdd %>%
+  tsibble::interval() %>%
+  class()
+
+sdd %>% tsibble::n_keys()
+sdd %>% tsibble::key()
+sdd %>% tsibble::key_data()
+sdd %>% tsibble::key_vars()
+sdd %>% tsibble::key_rows()
+
+sdd %>% tsibble::measures()
+sdd %>% tsibble::measured_vars()
+
+sdd %>% tsibble::append_case()
